7292c97063a9a9d56bb1b04754bb03f4a2f19cad
PapArturo <centinela90@gmail.com>
2026-08-13 21:40:24 -0600

    Update .env.example and query initialization to use DATABASE_URL for database connection.

--- app/utils/db/query.ts
+++ app/utils/db/query.ts
@@ -8,13 +8,7 @@ async function query<T extends QueryResultRow = any>(
 ): Promise<[CamelCaseKeys<T[], true>, number]> {
   noStore();
   try {
-    const client = new Client({
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: "collaboss",
-    });
+    const client = new Client({ connectionString: process.env.DATABASE_URL });
 
     await client.connect();
     const { rows, rowCount } = await client.query<T>(text, values);
